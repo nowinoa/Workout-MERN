ac81f0e5bac5a96faeb491de5ea5030bff6060cf
nowinoa <apt.code14@gmail.com>
2023-05-26 12:04:37 +0200

    adding json web tokens for signing up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,23 @@
 const User = require('../models/userModel')
 
+//* Importing json web tokens (JWS)
+const jwt = require('jsonwebtoken');
+
+//* Creating the tokens
+//Everytime we add a new user to the db, automatically an id is attached to it (access to the id by _id)
+const createToken = (_id) => {
+  //sign(payload) => the object with the id represents the payload (the user data that will be encoded to form the token)
+  // ? How does the token look like?
+   //it is a encoded version of header.payload.signature
+   // header: the algorithm used to encode by jwt
+   //payload: the data from the user (id, name, email...), identificators //! don't add sensitive data
+   //signature: a random string called "secret", this will be only known by the server
+   //if a user wants to log in and the secret doesn't match any signature, that person won't have access to the web
+
+  //payload: id / secret: a string hidded on the environment variables / expiress in 3 days: the user can be loged in up to three days then the token will expire
+  //* Token
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 // login a user
 const loginUser = async (req, res) => {
   res.json({mssg: 'login user'})
@@ -15,10 +33,15 @@ const signupUser = async (req, res) => {
     const user = await User.signup(email, password)
     //the signup method returns and object with the following structure: const user = await this.create({email, password: hash});
 
-    res.status(200).json({email, user})
+    // * Create a token for the user id from db
+    const token = createToken(user._id);
+    //would be something like = eyJhbGciOiJIUzI1NiInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDcwODNhNjE4ZDVhMWJmZDViZjQ2ODIiLCJpYXQiOjE2ODUwOTUzMzQsImVNTM1NDUzNH0.6uyP7lXKdQecN5NvKewEfQaDN4P9ATTSkegh5GWTw8"
+
+    res.status(200).json({email, token});
+    
   } catch (error) {
      //email already in use error and errors from mongoose (no email provided or no password)
-    res.status(400).json({error: 'Email is already in use'})
+     res.status(400).json({error: error.message})
   }
 }
 
